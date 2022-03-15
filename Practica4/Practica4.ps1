ce28f707441757d4692905fb3b50286a3229cb1c
Chochos13 <edgar.hernandezga@uanl.edu.mx>
2022-03-15 15:21:57 -0600

    first commit

--- Practica4/Practica4.ps1
+++ Practica4/Practica4.ps1
@@ -9,6 +9,6 @@ $statusdes = (Invoke-WebRequest https://discuss.zerotier.com).StatusDescription
 if ( ($status = 200) -and ($statusdes = "OK"))
 {
 	$j = (Invoke-WebRequest https://discuss.zerotier.com).content | Select-Object $j | Format-Table -HideTableHeaders | Out-File $ResultFile -Encoding ascii
-	$s = (Invoke-WebRequest https://discuss.zerotier.com).rawcontent | Select-Object $s | Format-Table -HideTableHeaders | Out-File $ResultFile1 -Encoding ascii
+	$s = (Invoke-WebRequest https://discuss.zerotier.com).RawContent | Select-Object $s | Format-Table -HideTableHeaders | Out-File $ResultFile1 -Encoding ascii
 	$k = Invoke-RestMethod https://discuss.zerotier.com -Method GET | Select-Object $k | Format-Table -HideTableHeaders | Out-File $ResultFile2 -Encoding ascii
 }
